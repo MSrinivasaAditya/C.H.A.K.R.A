f09bb7500b4fd3d6ecc7d505e2a71c2d11f215e4
M Srinivasa Aditya <msrinivasaditya.2204@gmail.com>
2026-03-17 09:33:08 +0530

    cleanup

--- target_repo_ui/user_code.py
+++
@@ -1,5 +0,0 @@
-// VULNERABLE: String concatenation
-String username = request.getParameter("username");
-String query = "SELECT secret FROM Users WHERE username = '" + username + "'";
-Statement statement = connection.createStatement();
-ResultSet result = statement.executeQuery(query);
